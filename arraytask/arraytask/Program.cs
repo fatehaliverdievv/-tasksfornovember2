345ebb255dc28e50cef04d31e2b2da4423815cfa
fatehaliverdievv <tu7l7qsfn@code.edu.az>
2022-11-01 18:00:26 +0400

    2 november tasks added

---
+++ arraytask/arraytask/Program.cs
@@ -0,0 +1,34 @@
+﻿#region
+//// task1
+//using System;
+//int[] array = { 5, 2, 3, 4 };
+//int min = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (i == 0)
+//    {
+//        min = array[i];
+//    }
+//    if (array[i] < min)
+//    {
+//        min = array[i];
+//    }
+//}
+//Console.WriteLine("massivin en kicik elementi :  " + min);
+#endregion
+
+//#region
+//// task2
+//using System;
+//int[] array = { 45, 2, 32, 4 };
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i]>=0 && array[i]<=9)
+//    {
+//        Console.WriteLine("reqem :" + array[i] );
+//    }
+//}
+#endregion
+
+
+
